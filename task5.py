a084a6e45e8bb7e6d05117649fbde88968c26f2d
PANDEY_ANOUJ <anoujpandey99911@gmail.com>
2025-11-07 14:58:08 +0900

    Delete task5.py

--- task5.py
+++
@@ -1,7 +0,0 @@
-def original_title(*titles):
-    for title in titles:
-        print("\nOriginal:", title)
-        formatted = title.title()
-        print("Formatted:", formatted)
-
-original_title("python PROGRAMMING basics", "THE GREAT GATSBY", "harry potter")
